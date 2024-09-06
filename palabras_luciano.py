11134f1e1238de2bec42c70f4faad79985a095ab
Luci1274 <luciano.gonzalez@alumnos.ispi4038.edu.ar>
2024-09-06 20:19:16 -0300

    inicio de la preación del ranking

--- palabras_luciano.py
+++ palabras_luciano.py
@@ -1,9 +1,9 @@
 from random import choice, shuffle
-def crear_archivo(ruta_de_archivo):
+def crear_archivo_de_jugadores(ruta_de_archivo):
     """La función crea y cierra un archivo de texto"""    
     try:
-        crear_archivo_personas = open(ruta_de_archivo, "x")
-        crear_archivo_personas.close()
+        crear_archivo_jugadores = open(ruta_de_archivo,"x")
+        crear_archivo_jugadores.close()
     except:
         print("El archivo ya fue creado")
     return
@@ -44,15 +44,33 @@ def juego(buscar_en_archivo):
 def final():
     final = print("Que tenga un buen día", "las veces jugadas fueron:",veces_jugadas , "\nLas veces acertadas fueron:",veces_acertadas,"\n Los reintentos fueron: ", reintentos, "\n sus puntos fueron", puntos)
     return
+
+def guardar_jugadores(ruta_de_jugadores, ingresar_jugadores, puntos):
+    jugadores = open(ruta_de_jugadores,"a")
+    jugadores.write(ingresar_jugadores)
+    jugadores.write("\t"*10)
+    jugadores.write(str(puntos) + "\n")
+    jugadores.close()
+    return
+
 #cuerpo
 veces_acertadas = 0
 veces_jugadas = 0
 reintentos = 0
 ruta_de_archivo = "/home/luci/Documentos/python/logica/palabras/spanish.lst"
-crear_archivo(ruta_de_archivo)
+ruta_de_jugadores = "/home/luci/Documentos/python/logica/palabras/jugadores.txt"
+crear_archivo_de_jugadores(ruta_de_jugadores)
 leer_archivo = open(ruta_de_archivo,"r")
-vidas = 5
 bienvenido = input("Bienvendio muchas gracias por probar este juego ¿Quieres continuar? ").strip()
+while True:
+    ingresar_jugador = input("Por favor ingrese un nombre de jugador: ").strip().upper()
+    if ingresar_jugador.isdigit:
+        ingresar_jugador == False
+        break
+    else:
+        print("Solo se permiten nombres")
+        continue
+
 if bienvenido:
     letras_totales,largo_minimo,largo_maximo,largo_promedio = buscar_palabras(bienvenido,leer_archivo)
     print(letras_totales)
@@ -61,7 +79,8 @@ if bienvenido:
     print(largo_promedio)
     buscar_en_archivo = open(ruta_de_archivo,"r")
     palabra_elegida, palabra_desordenada = juego(buscar_en_archivo)
-    print(palabra_desordenada)
+    print(palabra_desordenada, palabra_elegida)
+    vidas = 5
     while True:
         veces_jugadas += 1
         resolver = input("Por favor ordena esta palabra: ").upper().strip()
@@ -69,15 +88,17 @@ if bienvenido:
             puntos = vidas * len(palabra_elegida)
             veces_acertadas +=1
             print("\nMuy bien",palabra_elegida)
+            guardar_jugadores(ruta_de_jugadores, ingresar_jugador, puntos)
             final()
             break
         elif vidas == 0 :
-            print("\nperdiste (T_T)", palabra_elegida)
+            print("\nperdiste (T_T) la palabra era:", palabra_elegida)
             puntos = vidas * len(palabra_elegida)
+            guardar_jugadores(ruta_de_jugadores, ingresar_jugador, puntos)
             final()
             break
         elif resolver != palabra_elegida:
-            print("\nerror vuelva a intentar ordenar:", palabra_desordenada, "te quedan", vidas )
+            print("\nerror vuelva a intentar ordenar:", palabra_desordenada, "te quedan", vidas - 1)
             vidas -= 1
             reintentos +=1
             continue
