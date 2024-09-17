fbe47692321b778bbc0d803244b7262c9582f4e1
Luci1274 <luciano.gonzalez@alumnos.ispi4038.edu.ar>
2024-09-17 18:33:19 -0300

    prueba

--- palabras_revueltas_nueva_version.py
+++ palabras_revueltas_nueva_version.py
@@ -52,25 +52,24 @@ def promedio(ruta_tema_elegido):
             numero = minimo
     resta = maximo - minimo
     medio = resta / 2
+    print(minimo,maximo,medio)
     return ruta_tema_elegido,medio, maximo
 
-def mesclar_palabra(ruta_tema_elegido, ruta_de_jugador,promedio, maximo):
+def mesclar_palabra(ruta_tema_elegido, ruta_de_jugador,promedio):
     tema_elegido =open(ruta_tema_elegido,"r").readline().splitlines()
-    contador = 1
+    contador = 0
     while True:
-        if contador != 4:
+        if contador != 3:
             dificultad = input("Escribe una dificultas <FACIL> o <DIFICIL>: ").strip().upper()
             if not dificultad:
                 print("Por favor elija una")
                 continue
             elif dificultad == "FACIL":
-                while contador:
                     palabra = choice(tema_elegido).upper()
                     if len(palabra) <= int(promedio):
                         x = list(palabra)
                         shuffle(x)
                         palabra_elegida = palabra
-                        contador += 1
                         with open(ruta_de_jugador,"a")as dato_jugador:
                             dato_jugador.write(palabra_elegida)
                             dato_jugador.write(",")
@@ -80,11 +79,10 @@ def mesclar_palabra(ruta_tema_elegido, ruta_de_jugador,promedio, maximo):
             elif dificultad == "DIFICIL":
                 while contador:
                     palabra = choice(tema_elegido).upper()
-                    if len(palabra) <= int(maximo) and len(palabra) >= int(promedio):
+                    if len(palabra) >= int(promedio):
                         x = list(palabra)
                         shuffle(x)
                         palabra_elegida = palabra
-                        contador +=1
                         with open(ruta_de_jugador,"a")as dato_jugador:
                             dato_jugador.write(palabra_elegida + ",")
                         return palabra_elegida, x
@@ -96,14 +94,14 @@ def mesclar_palabra(ruta_tema_elegido, ruta_de_jugador,promedio, maximo):
 def juego_1(palabra_elegida, palabra_desordenada, ruta_de_jugador):
     intentos = 4
     print(palabra_desordenada)
+    puntos = intentos * len(palabra_elegida)
     while True:
         resolver = input("Por favor ordena esta palabra: ").upper().strip()
         if resolver == palabra_elegida:
-            puntos = intentos * len(palabra_elegida)
+            puntos
             print("\nMuy bien",palabra_elegida)
             return
         elif intentos == 1 :
-            puntos = intentos * len(palabra_elegida)
             print("\nperdiste (T_T) la palabra era:", palabra_elegida, "Tus puntso fueron", puntos)
             return
         elif resolver != palabra_elegida:
@@ -122,21 +120,16 @@ def ranking(ruta_de_jugadores):
     nombre = mejores_jugadores[0]
     tema = mejores_jugadores[1]
     puntos = mejores_jugadores[5]
-    for jugadores in mejores_jugadores:
-        jugadores = list(nombre + tema + puntos)
-        for jugadores in jugadores,range(10):
-            jugadores.sort[2]
-            enumerate(jugadores)
-            print(jugadores)
-        pausa = input("Precione <ENTER> para continuar")
-        return
+
+    pausa = input("Precione <ENTER> para continuar")
+    return
 
 
 
 #Cuerpo principal#
 #palabras/temas/marcas de autos.lst
-ruta_de_jugador = "/home/luci/Documentos/python/logica/palabras/archivo_jugador.lst"
-ruta_temas = "/home/luci/Documentos/python/logica/palabras/temas"
+ruta_de_jugador = "archivo_jugador.csv"
+ruta_temas = "temas"
 crear_archivo(ruta_de_jugador)
 while True:
     nombre_jugador = input("Bienvenido por favor ingrese su nombre o aprete <ENTER> para salir: ").strip()
@@ -156,9 +149,10 @@ ruta_tema_elegido,medio, maximo = promedio(ruta_tema_elegido)
 contador = 0
 while True:
     if contador != 3:
-        palabra_elegida, x = mesclar_palabra(ruta_tema_elegido, ruta_de_jugador,medio, maximo)
+        palabra_elegida, x = mesclar_palabra(ruta_tema_elegido, ruta_de_jugador,medio)
         juego_1(palabra_elegida, x,  ruta_de_jugador)
         contador +=1
         continue
     else:
         ranking(ruta_de_jugador)
+        exit()
